e18d3cbcf0a43f627397c49d518012d21d8180cf
Dinusanth Surendran <dinusanth.s@gmail.com>
2025-08-04 17:46:41 -0400

    Update ESLint config to be less strict for deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
